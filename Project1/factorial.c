16be9141493f76a2f80f70a4108dbbdfcc4c1049
mic050r <s2216@e-mirim.hs.kr>
2023-03-23 16:42:08 +0900

    실습 1-5 팩토리얼을 재귀호출로 구하기

---
+++ Project1/factorial.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int factorial(int n) {
+	if (n == 1) return 1;
+	return n * factorial(n - 1);
+}
+int main(void) {
+	int n = 5;
+	printf("%d!= %d \n", n, factorial(n));
+	return 0;
+}

1cae27cf082167e8f7e615bb1e61f3618882b6c7
mic050r <s2216@e-mirim.hs.kr>
2023-03-30 17:17:20 +0900

    하노이 탑 수행 코드

--- Project1/HanoiTower.c
+++ Project1/HanoiTower.c
@@ -3,7 +3,7 @@
 void HanoiTower(int n, char a, char b, char c) {
 	if (n == 1) printf("원판 %d, %c -> %c \n", n, a, c); // n : 원판 번호
 	else {
-		HanoiTower(n - 1, a, c, b); // (n-1)개를 출발지 -> 경유지
+		HanoiTower(n - 1, a, c, b); // (n-1)개를 출발지 -> 경유지(보조)
 		printf("원판 %d, %c -> %c \n", n, a, c); //n번 원판 이동
 		HanoiTower(n - 1, b, a, c); // (n-1)개를 경유지 -> 도착지
 	}
@@ -12,7 +12,7 @@ void HanoiTower(int n, char a, char b, char c) {
 int main(void) {
 
 	// 원판 개수
-	int n = 4;
+	int n = 8;
 	// A는 출발지, B는 경유지, C는 목적지의 명칭
 	HanoiTower(n, 'A', 'B', 'C');
 
